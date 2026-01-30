cf395d096cce215074864b7f6e42c1c901a12c47
user <prostreet1212@gmail.com>
2026-01-30 16:52:28 +0300

    baseURL/1/1.7/ex_build_context6/lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,70 +17,123 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-    debugPrint('Count value = $_counter');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    debugPrint('Run build MyHomePage widget');
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const MyTextInfo(
-              text: 'You have pushed the button this many times:',
+      home: const Scaffold(
+        body: Center(
+          child: MyColorBox(
+            color: Colors.blue,
+            child: MyColorBox(
+              color: Colors.red,
+              child: MyColorBox(
+                color: Colors.green,
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      MyButton(
+                        text: 'First parent',
+                        onPressed: firstParentChangeColor,
+                      ),
+                      SizedBox(height: 20),
+                      MyButton(
+                        text: 'Last parent',
+                        onPressed: lastParentChangeColor,
+                      )
+                    ],
+                  ),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(
-                context,
-              ).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
   }
 }
 
-class MyTextInfo extends StatelessWidget {
+// Функция для поиска самого первого родительского MyColorBoxState в
+// дереве виджетов (относительно контекста) для изменения цвета.
+void firstParentChangeColor(BuildContext context) {
+  final myColorBox = context.findAncestorStateOfType<_MyColorBoxState>();
+  if (myColorBox?._color == Colors.green) {
+    myColorBox?.changeColor(Colors.grey);
+  } else {
+    myColorBox?.changeColor(Colors.green);
+  }
+}
+
+// Функция для поиска самого верхнего родительского MyColorBoxState в
+// дереве виджетов для изменения цвета.
+void lastParentChangeColor(BuildContext context) {
+  final myColorBox = context.findRootAncestorStateOfType<_MyColorBoxState>();
+  if (myColorBox?._color == Colors.blue) {
+    myColorBox?.changeColor(Colors.brown);
+  } else {
+    myColorBox?.changeColor(Colors.blue);
+  }
+}
+
+class MyButton extends StatelessWidget {
+  final void Function(BuildContext context) onPressed;
   final String text;
-  const MyTextInfo({super.key, required this.text});
+  const MyButton({
+    super.key,
+    required this.onPressed,
+    required this.text,
+  });
 
   @override
   Widget build(BuildContext context) {
-    debugPrint('Run build MyTextInfo widget');
-    return Text(
-      text,
+    return ElevatedButton(
+      onPressed: () {
+        onPressed(context);
+      },
+      child: Text(text),
     );
+  }
+}
+
+class MyColorBox extends StatefulWidget {
+  static const padding = 30.0;
+  final Color color;
+  final Widget? child;
+
+  const MyColorBox({
+    super.key,
+    required this.color,
+    this.child,
+  });
+
+  @override
+  State<MyColorBox> createState() => _MyColorBoxState();
+}
+
+class _MyColorBoxState extends State<MyColorBox> {
+  Color _color = Colors.transparent;
+  Widget? _child;
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      _color = widget.color;
+      _child = widget.child;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(
+        MyColorBox.padding,
+      ),
+      color: _color,
+      child: _child,
+    );
+  }
+
+  void changeColor(Color color) {
+    setState(() {
+      _color = color;
+    });
   }
 }
