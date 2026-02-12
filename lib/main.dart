8f6ea2d0b21aaca2662a1fff0a47b642dbddeffe
user <prostreet1212@gmail.com>
2026-02-12 12:42:07 +0300

    fonts

--- lib/main.dart
+++ lib/main.dart
@@ -30,68 +30,66 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+enum CreditRating {
+  aaa('AAA'),
+  aaPlus('AA+'),
+  cMinus('C-');
+
+  final String text;
+  const CreditRating(this.text);
+}
+
 class _MyHomePageState extends State<MyHomePage> {
-  bool is24HourTime = true;
-  TimeOfDay? selectedTime; // выбранное пользователем время
+  CreditRating currentRating = CreditRating.aaa;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          const SizedBox(height: 100),
-          SwitchListTile(
-              title: Text(
-                is24HourTime ? '24-hour time' : '12-hour time',
-              ),
-              value: is24HourTime,
-              onChanged: (value) {
-                setState(() {
-                  is24HourTime = value;
-                });
-              }),
-          ElevatedButton(
-              child: const Text('Select time'),
-              onPressed: () async {
-                selectedTime = await showTimePicker(
-                  // Подпись в верху диалогового виджета
-                  barrierLabel: 'Select time',
-                  // передаем контекст в showTimePicker
-                  context: context,
-                  // устанавливаем начальное время
-                  initialTime: selectedTime ?? TimeOfDay.now(),
-                  // устанавливаем начальный режим
-                  initialEntryMode: TimePickerEntryMode.dial,
-                  // устанавливаем ориентацию
-                  orientation: Orientation.portrait,
-                  // устанавливаем формат времени
-                  builder: (context, child) {
-                    return MediaQuery(
-                      data: MediaQuery.of(context).copyWith(
-                        alwaysUse24HourFormat: is24HourTime,
+        body: Center(
+            child: Column(
+              children: [
+                const SizedBox(height: 60),
+                SegmentedButton<CreditRating>(
+                  showSelectedIcon: false,
+                  // список сегментов
+                    segments: <ButtonSegment<CreditRating>>[
+                      ButtonSegment<CreditRating>(
+                        // значение ButtonSegment
+                        value: CreditRating.aaa,
+                        // текст внутри ButtonSegment
+                        label: Text(CreditRating.aaa.text),
                       ),
-                      child: child!,
-                    );
-                  },
-                );
-                setState(() {
-                  // обновляем состояние
-                });
-              }),
-          const SizedBox(height: 100),
-          if (selectedTime != null)
-            Text(
-              'Selected time: ${selectedTime!.format(context)}',
-              style: const TextStyle(fontSize: 20),
-            )
-          else
-            const Text(
-              'No time selected',
-              style: TextStyle(fontSize: 20),
-            ),
-        ],
-      ),
-    );
+                      ButtonSegment<CreditRating>(
+                        value: CreditRating.aaPlus,
+                        label: Text(CreditRating.aaPlus.text),
+                      ),
+                      ButtonSegment<CreditRating>(
+                        value: CreditRating.cMinus,
+                        label: Text(CreditRating.cMinus.text),
+                      ),
+                      ButtonSegment<CreditRating>(
+                        value: CreditRating.cMinus,
+                        label: Text(CreditRating.cMinus.text),
+                      )
+                    ],
+                    selected: <CreditRating>{
+                      currentRating
+                    },
+                    onSelectionChanged: (Set<CreditRating> newSelection) {
+                      setState(() {
+                        // обновляем текущее значение рейтинга
+                        currentRating = newSelection.first;
+                      });
+                    }),
+                const SizedBox(height: 20),
+                Text(
+                  currentRating.text,
+                  style: const TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            )));
   }
 }
